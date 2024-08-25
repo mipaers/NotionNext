afd88f24c62a96431e2e8b9552f3acc63b742f52
mipaers <128981856+mipaers@users.noreply.github.com>
2024-08-25 15:15:31 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -45,7 +45,7 @@ const CONFIG = {
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
+  HEO_INFO_CARD_ICON1: 'fas fa-bad',
   HEO_INFO_CARD_URL2: 'https://github.com/mipaers',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: '/about',
